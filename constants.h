5035316b145a2e3d8cfe6de938942b27932078cd
Varrun Ramani <varrunr@gmail.com>
2012-04-29 14:02:11 -0400

    Added evaluation constants

--- constants.h
+++ constants.h
@@ -1,6 +1,13 @@
 #ifndef __CONSTANTS_H__
 #define __CONSTANTS_H__
 
+#define INFTY 10000
+#define KING INFTY
+#define LASER 9
+#define BLK 1 
+#define TRMIRR 3.5
+#define BMSPLTR 5
+
 typedef std::vector<int> coordinate;
 
 #endif
