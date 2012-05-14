037d93c04570a186775317b1f784497878bde63f
Shishir Horane <shishir.f22@gmail.com>
2012-05-13 21:11:39 -0400

    minor change

--- lchess.h
+++ lchess.h
@@ -6,7 +6,7 @@ namespace lchess
 	/* Function prototypes */
 	void fire( Piece& , Board& , int );
 	std::vector< mv::Move > get_next_moves( Board & , int);
-	mv::Move Game_Tree_Search(Board brd, int depth)
+	mv::Move Game_Tree_Search(Board brd, int depth);
 }
 
 #endif
